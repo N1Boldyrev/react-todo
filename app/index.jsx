cff454142c07f7cd80f187e1b13c77d3810f70f7
N1Boldyrev <n1boldyrev@yandex.ru>
2020-08-15 03:38:53 +0300

    Изменение файловой структуры

--- app/index.jsx
+++ app/index.jsx
@@ -5,8 +5,8 @@ import { store } from "./store/configureStore.js";
 import App from "./containers/App.jsx";
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById("root")
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
 );
